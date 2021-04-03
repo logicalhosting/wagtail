6380fd26ad8011f45155181e681c083fe5a849fc
logicalphase <john@logicalphase.com>
2021-04-03 01:34:13 +0000

    Remove blog for better option

--- blog/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-
-# AT THE TOP OF THE FILE
--- corporate/static_src/tailwind.config.js
+++ corporate/static_src/tailwind.config.js
@@ -1,26 +1,25 @@
 // This is a minimal config.
 // If you need the full config, get it from here:
 // https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
-const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     purge: [
         // Templates within theme app (e.g. base.html)
         '../templates/**/*.html',
         // Templates in other apps. Uncomment the following line if it matches
         // your project structure or change it to match.
-        '../../templates/**/*.html',
-        '../../home/templates/**/*.html',
+        // '../../templates/**/*.html',
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
     },
     variants: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
-        },
+        extend: {},
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 }
