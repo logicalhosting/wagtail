12995c0c7205c27e16516dc8ef95171006ff85b7
logicalphase <john@logicalphase.com>
2021-04-23 04:53:42 +0000

    added more news article fields

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+class PersonBlock(blocks.StructBlock):
+    first_name = blocks.CharBlock(max_length=128)
+    surname = blocks.CharBlock(max_length=256)
+    photo = ImageChooserBlock(required=False)
+    biography = blocks.RichTextBlock()
+
+    class Meta:
+        template = 'blocks/person_block.html'
+        icon = 'user'
--- news/models.py
+++ news/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField, StreamField
+from wagtail.embeds.blocks import EmbedBlock
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
@@ -14,6 +16,8 @@ from wagtailsvg.models import Svg
 from wagtailsvg.blocks import SvgChooserBlock
 from wagtailsvg.edit_handlers import SvgChooserPanel
 
+from core.models import PersonBlock
+
 
 # The decorator registers this model as a news index
 @newsindex
@@ -21,26 +25,37 @@ class NewsIndex(NewsIndexMixin, Page):
     # Add extra fields here, as in a normal Wagtail Page class, if required
     newsitem_model = 'NewsItem'
 
-
 class NewsItem(AbstractNewsItem):
     # NewsItem is a normal Django model, *not* a Wagtail Page. RichTextField
     # Add any fields required for your page.
     # It already has ``date`` field, and a link to its parent ``NewsIndex`` Page
-    title = models.CharField(max_length=255)
+    headline = models.CharField(max_length=255)
+    byline = models.CharField(max_length=644)
+    lead = StreamField([
+        ('lead', blocks.RichTextBlock()),
+    ], blank=True)
     body = StreamField([
         ('heading', blocks.CharBlock(form_classname="full title")),
-        ('svg', SvgChooserBlock()),
         ('paragraph', blocks.RichTextBlock()),
+        ('blockquote', blocks.BlockQuoteBlock()),
+        ('svg', SvgChooserBlock()),
         ('image', ImageChooserBlock()),
+        ('embed', EmbedBlock()),
+    ], blank=True)
+    author = StreamField([
+        ('author', PersonBlock()),
     ], blank=True)
 
     panels = [
-        FieldPanel('title', classname='full title'),
+        FieldPanel('headline', classname='full title'),
+        FieldPanel('byline', classname='full title'),
+        StreamFieldPanel('lead', classname='full lead'),
         StreamFieldPanel('body', classname='full'),
+        StreamFieldPanel('author', classname='full'),
     ] + AbstractNewsItem.panels
 
     def __str__(self):
-        return self.title
+        return self.headline
 
 
 class NewsItemRevision(AbstractNewsItemRevision):
