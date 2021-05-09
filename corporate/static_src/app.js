cd82e8f8b4800bc780d8683a0831ac0e559a7c6c
logicalphase <john@logicalphase.com>
2021-05-08 20:58:22 -0500

    Adding @material web

---
+++ corporate/static_src/app.js
@@ -0,0 +1,5 @@
+import '@material/mwc-button/index';
+import '@material/mwc-card/index';
+import '@material/mwc-textfield';
+import '@material/mwc-top-app-bar/index';
+import '@material/mwc-typography/index';
